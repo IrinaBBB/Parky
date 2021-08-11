635aeb3d25f0f29fd89993a79b9bca8fc29fd52b
Irina <balkarovairina@gmail.com>
2021-08-11 14:42:59 +0200

    add: HttpClient Config and SD.cs

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:5001/api/"; 
+        public static string NationalParkAPIPath = APIBaseUrl + "nationalparks"; 
+        public static string TrailAPIPath = APIBaseUrl + "trails"; 
+    }
+}
